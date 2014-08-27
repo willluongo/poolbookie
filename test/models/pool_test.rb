730e7194c9059f09ec1aabf586c144ae8c2417b8
Will Luongo <me@willluongo.com>
2014-08-27 17:56:12 -0400

    All tests pass now

--- test/models/pool_test.rb
+++ test/models/pool_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PoolTest < ActiveSupport::TestCase
   test "#buy_in should error when less than 1" do
-    refute Pool.new(buy_in: -1).valid?, 'buy_in must be greater than 0'
-    refute Pool.new(buy_in: 0).valid?, 'buy_in must be greater than 0'
+    refute Pool.new(wager_cents: -1).valid?, 'buy_in must be greater than 0'
+    refute Pool.new(wager_cents: 0).valid?, 'buy_in must be greater than 0'
   end
 end
