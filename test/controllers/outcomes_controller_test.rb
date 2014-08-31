f2d61b00d6e0d757b7ec174d419180e3f78ef7b0
Will Luongo <me@willluongo.com>
2014-08-31 11:48:27 -0400

    Fix most tests

--- test/controllers/outcomes_controller_test.rb
+++ test/controllers/outcomes_controller_test.rb
@@ -12,11 +12,13 @@ class OutcomesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    sign_in users(:one)
     get :new
     assert_response :success
   end
 
   test "should create outcome" do
+    sign_in users(:one)
     assert_difference('Outcome.count') do
       post :create, outcome: { summary: @outcome.summary,
         title: Array.new(32){rand(36).to_s(36)}.join, pool_id: rand(50) }
@@ -31,16 +33,19 @@ class OutcomesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:one)
     get :edit, id: @outcome
     assert_response :success
   end
 
   test "should update outcome" do
+    sign_in users(:one)
     patch :update, id: @outcome, outcome: { summary: @outcome.summary, title: @outcome.title }
     assert_redirected_to outcome_path(assigns(:outcome))
   end
 
   test "should destroy outcome" do
+    sign_in users(:one)
     assert_difference('Outcome.count', -1) do
       delete :destroy, id: @outcome
     end
--- test/controllers/pools_controller_test.rb
+++ test/controllers/pools_controller_test.rb
@@ -12,6 +12,7 @@ class PoolsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    sign_in users(:one)
     get :new
     assert_response :success
   end
@@ -30,16 +31,19 @@ class PoolsControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:one)
     get :edit, id: @pool
     assert_response :success
   end
 
   test "should update pool" do
+    sign_in users(:one)
     patch :update, id: @pool, pool: { wager_cents: @pool.wager_cents, title: @pool.title }
     assert_redirected_to pool_path(assigns(:pool))
   end
 
   test "should destroy pool" do
+    sign_in users(:one)
     assert_difference('Pool.count', -1) do
       delete :destroy, id: @pool
     end
