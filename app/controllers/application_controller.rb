30d83f171c3c875849b4aa7a539776477836a7b1
Will Luongo <me@willluongo.com>
2014-08-31 01:29:58 -0400

    Improve views, improve user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+private
+  def authenticate_admin!
+    unless current_user && current_user.isAdmin?
+      flash[:alert] = "You must be an admin to do that!"
+      redirect_to :root
+    end
+  end
 end
